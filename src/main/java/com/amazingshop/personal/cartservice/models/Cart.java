11c7ae7b9060b7265730925ec82011fefd7183f1
Matvey Shavlov <m.shavloff@gmail.com>
2025-07-15 14:47:03 +0300

    Add entity cart and table in db

--- src/main/java/com/amazingshop/personal/cartservice/models/Cart.java
+++ src/main/java/com/amazingshop/personal/cartservice/models/Cart.java
@@ -1,4 +1,35 @@
 package com.amazingshop.personal.cartservice.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Cart")
+@Data
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    @NotNull(message = "User ID cannot be null")
+    private Long userId;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @NotNull(message = "Created date cannot be null")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    @NotNull(message = "Updated date cannot be null")
+    private LocalDateTime updatedAt;
+
 }
