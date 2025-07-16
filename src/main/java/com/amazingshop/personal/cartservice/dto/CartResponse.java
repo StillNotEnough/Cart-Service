c9879c58f245daf88655543fe1e4308596383925
Matvey Shavlov <m.shavloff@gmail.com>
2025-07-16 14:44:32 +0300

    Add CartService code: make main methods for CartController

--- src/main/java/com/amazingshop/personal/cartservice/dto/CartResponse.java
+++ src/main/java/com/amazingshop/personal/cartservice/dto/CartResponse.java
@@ -1,6 +1,5 @@
 package com.amazingshop.personal.cartservice.dto;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
--- src/main/java/com/amazingshop/personal/cartservice/services/CartService.java
+++ src/main/java/com/amazingshop/personal/cartservice/services/CartService.java
@@ -1,8 +1,13 @@
 package com.amazingshop.personal.cartservice.services;
 
 import com.amazingshop.personal.cartservice.dto.CartRequest;
+import com.amazingshop.personal.cartservice.dto.CartResponse;
+import com.amazingshop.personal.cartservice.dto.ProductDTO;
 import com.amazingshop.personal.cartservice.models.Cart;
+import com.amazingshop.personal.cartservice.models.CartItem;
+import com.amazingshop.personal.cartservice.repositories.CartItemRepository;
 import com.amazingshop.personal.cartservice.repositories.CartRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +15,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Optional;
-
 @Service
 @Transactional(readOnly = true)
 public class CartService {
 
     private final CartRepository cartRepository;
     private final RestTemplate restTemplate;
+    private final CartItemRepository cartItemRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public CartService(CartRepository cartRepository, RestTemplate restTemplate) {
+    public CartService(CartRepository cartRepository, RestTemplate restTemplate, CartItemRepository cartItemRepository, ModelMapper modelMapper) {
         this.cartRepository = cartRepository;
         this.restTemplate = restTemplate;
+        this.cartItemRepository = cartItemRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Transactional
@@ -36,71 +43,62 @@ public class CartService {
         }
 
         // Проверка товара
-        ResponseEntity<>
+        ResponseEntity<ProductDTO> productResponse = restTemplate.getForEntity(
+          "http://product-service/products/", ProductDTO.class);
+        if (userResponse.getStatusCode() != HttpStatus.OK){
+            throw new IllegalArgumentException("Товар не найден");
+        }
 
+        ProductDTO product = productResponse.getBody();
 
+        if (product.getStock() < request.getQuantity()){
+            throw new IllegalArgumentException("Недостаточно товара на складе");
+        }
+
+        // Ищем или создаем новую корзину
+        Cart cart = cartRepository.findByUserId(request.getUserId())
+                .orElseGet(() -> {
+                   Cart newCart = new Cart();
+                   newCart.setUserId(request.getUserId());
+                   return newCart;
+                });
+
+        // Чтобы явно указать что это финальная версия cart
+        Cart finalCart = cart;
+
+        // Проверяем или создаем CartItem
+        CartItem cartItem = cartItemRepository.findCartItemByCartIdAndProductId(cart.getId(),
+                request.getProductId()).orElseGet(() -> {
+           CartItem newCartItem = new CartItem();
+           newCartItem.setCart(finalCart);
+           newCartItem.setProductId(request.getProductId());
+           newCartItem.setUnitPrice(product.getPrice());
+           return newCartItem;
+        });
+        cartItem.setQuantity(request.getQuantity());
+
+        // Добавить или обновить CartItem
+        if (cartItem.getId() == null){
+            cart.getCartItems().add(cartItem);
+        }
+        cart = cartRepository.save(cart);
+        return cart;
     }
 
     @Transactional
     public void clearCart(Long cartId) {
-        cartRepository.deleteById(cartId);
+        Cart cart = cartRepository.findById(cartId).orElseThrow(() ->
+                new IllegalArgumentException("Корзина не найдена"));
+        cart.getCartItems().clear();
+        cartRepository.save(cart);
     }
 
-    public Cart getCart(Long userId) {
-        Optional<Cart> cart = cartRepository.findByUserId(userId);
-
-        return cart.orElseThrow(); // todo what exception should be throw
+    public Cart getCart(Long cartId){
+        return cartRepository.findByUserId(cartId).orElseThrow(() ->
+                new IllegalArgumentException("Корзина не найдена"));
     }
 
-    // Метод 1: Добавить товар в корзину
-    @Transactional
-    public CartResponse addTfoCart(CartRequest request) {
-        // Проверка пользователя
-        ResponseEntity<String> userResponse = restTemplate.getForEntity(
-                "http://auth-service/users/" + request.getUserId(), String.class);
-        if (userResponse.getStatusCode() != HttpStatus.OK) {
-            throw new IllegalArgumentException("Пользователь не найден");
-        }
-
-        // Проверка товара
-        ResponseEntity<ProductDto> productResponse = restTemplate.getForEntity(
-                "http://product-service/products/" + request.getProductId(), ProductDto.class);
-        if (productResponse.getStatusCode() != HttpStatus.OK || productResponse.getBody() == null) {
-            throw new IllegalArgumentException("Товар не найден");
-        }
-        ProductDto product = productResponse.getBody();
-        if (product.getStock() < request.getQuantity()) {
-            throw new IllegalArgumentException("Недостаточно товара на складе");
-        }
-
-        // Поиск или создание корзины
-        Cart cart = cartRepository.findByUserId(request.getUserId())
-                .orElseGet(() -> {
-                    Cart newCart = new Cart();
-                    newCart.setUserId(request.getUserId());
-                    return newCart;
-                });
-
-        // Проверка существующего CartItem
-        CartItem cartItem = cartItemRepository.findByCartIdAndProductId(cart.getId(), request.getProductId())
-                .orElseGet(() -> {
-                    CartItem newItem = new CartItem();
-                    newItem.setCart(cart);
-                    newItem.setProductId(request.getProductId());
-                    newItem.setUnitPrice(product.getPrice());
-                    return newItem;
-                });
-        cartItem.setQuantity(request.getQuantity());
-
-        // Добавление или обновление CartItem
-        if (cartItem.getId() == null) {
-            cart.getItems().add(cartItem);
-        }
-        cart = cartRepository.save(cart);
-
-        return toCartResponse(cart);
+    public CartResponse convertedToCartResponse(Cart cart){
+        return modelMapper.map(cart, CartResponse.class);
     }
-
 }
-
-
