0ff42043129d03d13c2f02d54cfd556883fc38ff
Matvey Shavlov <m.shavloff@gmail.com>
2025-07-16 10:42:59 +0300

    Add dependency Model mapper and and 2 beans in main class: bean ModelMapper and RestTemplate

--- src/main/java/com/amazingshop/personal/cartservice/CartServiceApplication.java
+++ src/main/java/com/amazingshop/personal/cartservice/CartServiceApplication.java
@@ -1,13 +1,25 @@
 package com.amazingshop.personal.cartservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CartServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CartServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CartServiceApplication.class, args);
+    }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
