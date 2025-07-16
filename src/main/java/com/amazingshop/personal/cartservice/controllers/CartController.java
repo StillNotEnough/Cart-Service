bb38f4568ad0452d4cf7336b099b3d6336d1d76e
Matvey Shavlov <m.shavloff@gmail.com>
2025-07-16 15:03:57 +0300

    Add CartController with methods: clearCart, getCart, addToCart

--- src/main/java/com/amazingshop/personal/cartservice/controllers/CartController.java
+++ src/main/java/com/amazingshop/personal/cartservice/controllers/CartController.java
@@ -1,9 +1,11 @@
 package com.amazingshop.personal.cartservice.controllers;
 
+import com.amazingshop.personal.cartservice.dto.CartRequest;
+import com.amazingshop.personal.cartservice.dto.CartResponse;
 import com.amazingshop.personal.cartservice.services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carts")
@@ -14,5 +16,21 @@ public class CartController {
     @Autowired
     public CartController(CartService cartService) {
         this.cartService = cartService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CartResponse> addToCart(@RequestBody CartRequest request){
+        return ResponseEntity.ok(cartService.convertedToCartResponse(cartService.addToCart(request)));
+    }
+
+    @GetMapping("/{cartsId}")
+    public ResponseEntity<CartResponse> getCart(@PathVariable Long cartsId){
+        return ResponseEntity.ok(cartService.convertedToCartResponse(cartService.getCart(cartsId)));
+    }
+
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity<Void> clearCart(@PathVariable Long cartId){
+        cartService.clearCart(cartId);
+        return ResponseEntity.noContent().build();
     }
 }
